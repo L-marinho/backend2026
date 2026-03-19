07add0ff519bf38f66cb63c3d5569279dfd7de18
L-marinho <lucianopmarinho@gmail.com>
2026-03-19 00:29:15 +0000

    aula 18/03/26

--- src/index.php
+++ src/index.php
@@ -1,5 +1,20 @@
-<h1>Back-End</h1>
+<!DOCTYPE html>
+<html lang ="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORMULARIO-18/03/2026</title>
+</head> 
 
-<?php
+<body>
+    <style>
+        textPath
+    </style>
+    
+</body>
 
-phpinfo();
+</html><form method="post" action="recebe php">
+    login<imput type="password" id="login" name="login"><br>
+    senha<imput type="password" id="senha" name="senha"><br>
+    <imput type="submit" value="validar">
+</form>
